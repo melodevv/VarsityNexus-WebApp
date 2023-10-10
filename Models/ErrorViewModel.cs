5b76a7e2d7ea890701a6d2c7ac03c5bf7b1351f5
Tlhokomelo Direko <melodevify@gmail.com>
2023-10-10 16:05:15 +0200

    Add Project Files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VarsityNexusApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
